037d0d3f4de250d3e2e6d71993692971ec32464b
Tateishi Tadatoshi <ishio39@gmail.com>
2021-07-17 17:30:58 +0900

    make skeleton

---
+++ sudoku/__init__.py
@@ -0,0 +1,8 @@
+from . import cli
+from . import core
+
+
+VERSION = '0.0.1'
+
+
+main = cli.main
---
+++ sudoku/cli.py
@@ -0,0 +1,9 @@
+from . import core
+
+
+def main():
+    print("This is a skeleton.")
+
+
+if __name__ == "__main__":
+    main()
