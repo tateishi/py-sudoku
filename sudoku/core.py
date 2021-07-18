af377be2d89b8482a839a6b7bc9d4afb587cfeeb
Tateishi Tadatoshi <ishio39@gmail.com>
2021-07-18 15:31:58 +0900

    change: type names
    Cell -> CellBasic
    Sudoku -> Cell
    List[Sudoku] -> Sudoku

--- sudoku/core.py
+++ sudoku/core.py
@@ -72,10 +72,10 @@ def blk_from_pos(p: Pos) -> Peer:
 def peer_from_pos(p: Pos) -> Peer:
     return col_from_pos(p) | row_from_pos(p) | blk_from_pos(p)
 
-class Cell(NamedTuple):
+class CellBasic(NamedTuple):
     is_fixed: bool = False
     number: int = 0
-    memo: Set[int] = {}
+    memo: Set[int] = set()
 
     def __str__(self):
         if self.is_fixed:
@@ -84,63 +84,69 @@ class Cell(NamedTuple):
             return f'[{"".join([str(n) for n in self.memo])}]'
 
     def set_number(self, number: int):
-        return cell_init(number)
+        return cell_basic_init(number)
 
     def remove_memo(self, memo: int):
         m = self.memo.copy()
-        return cell_from_memo(m - {memo})
+        return cell_basic_from_memo(m - {memo})
 
     def remove_memos(self, memos: Set[int]):
         m = self.memo.copy()
-        return cell_from_memo(m - memos)
+        return cell_basic_from_memo(m - memos)
 
-def cell_init(number=0):
+def cell_basic_init(number=0):
     if number > 0:
-        return Cell(is_fixed=True, number=number)
+        return CellBasic(is_fixed=True, number=number)
     else:
-        return Cell(memo=set(range(1,10)))
+        return CellBasic(memo=set(range(1,10)))
 
-def cell_from_memo(memo: Set[int]) -> Cell:
-    return Cell(memo=memo)
+def cell_basic_from_memo(memo: Set[int]) -> CellBasic:
+    return CellBasic(memo=memo)
+
+
+class Cell(NamedTuple):
+    pos: Pos
+    cell: CellBasic
 
 
 class Sudoku(NamedTuple):
-    pos: Pos
-    cell: Cell
+    cells: List[Cell]
 
-def set_at(sudoku: List[Sudoku], p: Pos, n: int) -> List[Sudoku]:
-    def el(sudoku: Sudoku, p: Pos, n: int) -> Sudoku:
-        if sudoku.cell.is_fixed:
-            return sudoku
-        elif p == sudoku.pos:
-            return Sudoku(sudoku.pos, cell_init(n))
-        elif sudoku.pos.idx in peer_from_pos(p):
-            return Sudoku(sudoku.pos, sudoku.cell.remove_memo(n))
+
+def set_at(sudoku: Sudoku, p: Pos, n: int) -> Sudoku:
+    def el(cell: Cell, p: Pos, n: int) -> Cell:
+        if cell.cell.is_fixed:
+            return cell
+        elif p == cell.pos:
+            return Cell(cell.pos, cell_basic_init(n))
+        elif cell.pos.idx in peer_from_pos(p):
+            return Cell(cell.pos, cell.cell.remove_memo(n))
         else:
-            return sudoku
-    return [el(s, p, n) for s in sudoku]
+            return cell
+    return Sudoku([el(s, p, n) for s in sudoku.cells])
+
 
 def sudoku_load(game: str) -> Sudoku:
     from functools import reduce
-    def put_at(sudoku: List[Sudoku], cell: (int, str)) -> List[Sudoku]:
+    def put_at(sudoku: Sudoku, cell: (int, str)) -> Sudoku:
         p, c = cell
         if c in '123456789':
             return set_at(sudoku, Pos(p), int(c))
         return sudoku
-    sudoku = [Sudoku(Pos(n), cell_init(0)) for n in range(81)]
+    sudoku = Sudoku([Cell(Pos(n), cell_basic_init(0)) for n in range(81)])
     problem = [c for c in game if c in '123456789.']
 
     return reduce(put_at, enumerate(problem), sudoku)
 
-def pprint(sudoku: List[Sudoku]):
-    width = max(3, max(len(str(s.cell)) for s in sudoku))
+def pprint(sudoku: Sudoku):
+    width = max(3, max(len(str(s.cell)) for s in sudoku.cells))
     for y in range(9):
         if y > 0 and y % 3 == 0:
             print('---' * width + '+' + '---' * width + '+' + '---' * width)
         for x in range(9):
             if x > 0 and x % 3 == 0:
                 print('|', end='')
-            print(f'{str(sudoku[y*9+x].cell):^{width}}', end='')
+            print(f'{str(sudoku.cells[y*9+x].cell):^{width}}', end='')
         print()
 
 
@@ -152,12 +158,12 @@ class SingleCandidate(NamedTuple):
         return f'{self.pos.idx}, {self.number}'
 
 
-def find_single_candidate(sudoku: List[Sudoku]) -> List[SingleCandidate]:
+def find_single_candidate(sudoku: Sudoku) -> List[SingleCandidate]:
     return [SingleCandidate(s.pos, s.cell.memo.copy().pop())
-            for s in sudoku
+            for s in sudoku.cells
             if len(s.cell.memo) == 1]
 
 
-def apply_single_candidates(sudoku: List[Sudoku], candidates: List[SingleCandidate]) -> List[Sudoku]:
+def apply_single_candidates(sudoku: Sudoku, candidates: List[SingleCandidate]) -> Sudoku:
     from functools import reduce
     return reduce(lambda s, c: set_at(s, c.pos, c.number), candidates, sudoku)
