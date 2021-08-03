5bf4995b481c70894355cf848271a3ecbdf105f1
Tateishi Tadatoshi <ishio39@gmail.com>
2021-08-03 18:06:05 +0900

    correct annotations

--- sudoku/model/peer.py
+++ sudoku/model/peer.py
@@ -1,20 +1,21 @@
 from __future__ import annotations
+from typing import Set
 from dataclasses import dataclass
 
 from . import Pos
 
 
-@dataclass
+@dataclass(frozen=True)
 class Peer:
-    peer: Set[Pos]
+    peer: Set[int]
 
     def __str__(self):
         return f'[{",".join(str(p) for p in sorted(self.peer))}]'
 
-    def __or__(self, p):
+    def __or__(self, p: Peer) -> Peer:
         return Peer(self.peer | p.peer)
 
-    def __and__(self, p):
+    def __and__(self, p: Peer) -> Peer:
         return Peer(self.peer & p.peer)
 
     def __contains__(self, n: int) -> bool:
@@ -24,35 +25,35 @@ class Peer:
         return len(self.peer)
 
     @classmethod
-    def col(cls, p) -> Peer:
+    def col(cls, p: int | Pos) -> Peer:
         if isinstance(p, int):
-            return Peer({i * 9 + p for i in range(9)})
+            return cls({i * 9 + p for i in range(9)})
         elif isinstance(p, Pos):
-            return Peer.col(p.x)
+            return cls.col(p.x)
         else:
             raise TypeError
 
     @classmethod
-    def row(cls, p) -> Peer:
+    def row(cls, p: int | Pos) -> Peer:
         if isinstance(p, int):
-            return Peer({p * 9 + i for i in range(9)})
+            return cls({p * 9 + i for i in range(9)})
         elif isinstance(p, Pos):
-            return Peer.row(p.y)
+            return cls.row(p.y)
         else:
             raise TypeError
 
     @classmethod
-    def blk(cls, p) -> Peer:
+    def blk(cls, p: int | Pos) -> Peer:
         if isinstance(p, int):
             r = p // 3
             c = p % 3
             b = r * 27 + c * 3
-            return Peer({b + i for i in (0, 1, 2, 9, 10, 11, 18, 19, 20)})
+            return cls({b + i for i in (0, 1, 2, 9, 10, 11, 18, 19, 20)})
         elif isinstance(p, Pos):
-            return Peer.blk(p.blk)
+            return cls.blk(p.blk)
         else:
             raise TypeError
 
     @classmethod
-    def peers(cls ,p: Pos) -> Peer:
-        return Peer.col(p) | Peer.row(p) | Peer.blk(p)
+    def peers(cls, p: Pos) -> Peer:
+        return cls.col(p) | cls.row(p) | cls.blk(p)
